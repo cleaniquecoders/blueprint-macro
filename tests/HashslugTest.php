dbfb6bf95c66a021a8a9baa99a3a209c2a91a406
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-26 13:07:10 +0800

    Added hashslug test.

---
+++ tests/HashslugTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CLNQCDRS\Blueprint\Macro\Tests;
+
+use CLNQCDRS\Blueprint\Macro\Tests\TestCase;
+use Illuminate\Support\Facades\Schema;
+
+class HashslugTest extends TestCase
+{
+    /** @test */
+    public function it_has_hashslug_table()
+    {
+        $this->assertTrue(Schema::hasTable('hashslug'));
+    }
+
+    /** @test */
+    public function it_has_hashslug_column()
+    {
+        $this->assertTrue(Schema::hasColumn('hashslug', 'hashslug'));
+    }
+}
